d095ce5da8160b94a46c61d51a761ad321df715e
Arturo Torres <dvluistorres@gmail.com>
2022-09-20 13:39:13 -0500

    Add styles and working buttons, Fix typo, Add drawing code

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
 
 const sketch = document.querySelector('#sketch')
 
+/**To revome the squares befor creating another */
 function removeElementsByClass(className){
     const elements = document.getElementsByClassName(className);
     while(elements.length > 0){
@@ -11,8 +12,50 @@ function removeElementsByClass(className){
     }
 }
 
+/**To check if the mouse is pressed down or not */
+let mouseDown = 0;
+document.body.onmousedown = function() { 
+  ++mouseDown;
+}
+document.body.onmouseup = function() {
+  mouseDown = 0;
+}
+
+/**To select the color */
+let col = "black"
+function generateRandomColor(){
+    let maxVal = 0xFFFFFF; // 16777215
+    let randomNumber = Math.random() * maxVal; 
+    randomNumber = Math.floor(randomNumber);
+    randomNumber = randomNumber.toString(16);
+    let randColor = randomNumber.padStart(6, 0);   
+    return `#${randColor.toUpperCase()}`
+}
+const buttons = document.getElementsByClassName('button');
+for (const button of buttons) {
+    button.addEventListener('click', function onClick() {
+        switch (this.id) {
+            case "black":
+                col = "black";
+                break;
+            case "red":
+                col = "red"
+                break;
+            case "random":
+                col = generateRandomColor();
+                break;
+        }
+    });
+  }
+
+
+
+/** create divs equal to NxN */
 function createSketch (size){
     size = parseInt(document.getElementById("size").value);
+    if (size>50) {
+        alert("50 is the limit");
+    } else {
     const squares = document.getElementsByClassName("square");
     if (squares.length != 0) {
         removeElementsByClass('square');
@@ -20,28 +63,39 @@ function createSketch (size){
     for (let i=0 ; i < size**2 ; i++){
         const square = document.createElement('div');
         square.setAttribute('class', 'square');
+        square.setAttribute('draggable', 'false');
         sketch.appendChild(square);
     }
+    /** to set the size of the inner squares */
     const nsquares = document.getElementsByClassName("square");
     let sketchWidth = sketch.offsetWidth
-    let ssize = Math.floor(sketchWidth/size);
+    let ssize = (sketchWidth/size);
     for(let j=0 ; j<nsquares.length ;j++){
-        nsquares[j].style.width = `${Math.floor(ssize)-3}px`;
-        nsquares[j].style.height = `${Math.floor(ssize)-3}px`;
+        nsquares[j].style.width = `${(ssize)*0.999}px`;
+        nsquares[j].style.height = `${(ssize)*0.999}px`;
+        /** To paint if it's clicked*/
+        nsquares[j].addEventListener('mouseover', function hopping(event) {
+            if(mouseDown>0){
+            this.style.backgroundColor = `${col}`}
+        });
     }
-}
+}}
 
+/** for refreshing the little squares size when the window changes width */
 function refresh() {
     const nsquares = document.getElementsByClassName("square");
     let sketchWidth = sketch.offsetWidth;
     size = parseInt(document.getElementById("size").value);
     let ssize = Math.floor(sketchWidth/size);
     for(let j=0 ; j<nsquares.length ;j++){
-        nsquares[j].style.width = `${Math.floor(ssize)-3}px`;
-        nsquares[j].style.height = `${Math.floor(ssize)-3}px`;
+        nsquares[j].style.width = `${(ssize)*0.999}px`;
+        nsquares[j].style.height = `${(ssize)*0.999}px`;
+              
     }
 }
 
 window.addEventListener('resize', function () {
+    
     refresh();
 })
+
